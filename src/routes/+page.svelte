481d681514ce60a7b7a6bfbe3125ea311f700a8f
devjoeween <jjazan201@gmail.com>
2023-01-05 23:39:45 +0900

    feat: add header in path /
    * add favicon.ico
    * add README.md
    * add base style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Header />
+
+<script>
+import Header from './Header.svelte';
+</script>
+
+<style>
+    :global(body) {
+        background-color: #fdfeff;
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,55 @@
+<div class="root">
+    <div class="logo">
+        <img
+            class="img"
+            src="/images/logo_black_transparent.png"
+            width=32
+            height=32
+            alt="로고"
+        >
+        <span class="text">DEVJAEWON</span>
+    </div>
+    <div class="link-records">
+        <a class="link" href="/">기록</a>
+    </div>
+</div>
+
+<style>
+.root {
+    height: 60px;
+    padding: 14px;
+}
+.root::after {
+    content: '';
+    display: block;
+    clear: both;
+}
+.logo {
+    float: left;
+    font-size: 0;
+}
+.logo .img {
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    vertical-align: top;
+}
+.logo .text {
+    display: inline-block;
+    margin-left: 4px;
+    font-size: 20px;
+    line-height: 32px;
+    vertical-align: top;
+}
+.link-records {
+    float: right;
+}
+.link-records .link {
+    display: block;
+    padding: 0 14px;
+    border-radius: 2px;
+    background-color: #e3e3e3;
+    font-size: 15px;
+    line-height: 32px;
+}
+</style>
