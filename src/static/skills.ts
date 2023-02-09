af2b8d2b04a7a23cf3abe66f6373ca53efd0c950
devjoeween <jjazan201@gmail.com>
2023-02-09 21:25:21 +0900

    chore: sort skill variables

--- src/static/skills.ts
+++ src/static/skills.ts
@@ -1,32 +1,32 @@
-export const GOLANG = "golang";
-export const FLUTTER = "flutter";
-export const GOOGLE_CLOUD = "google_cloud";
+export const CHROMIUM = "chromium";
+export const CSS = "css";
+export const CYPRESS = "cypress";
 export const ECHO = "echo";
+export const ELASTIC = "elastic";
+export const FLUTTER = "flutter";
+export const GATSBY = "gatsby";
+export const GITHUB_ACTIONS = "github_actions";
+export const GOLANG = "golang";
+export const GOOGLE_CLOUD = "google_cloud";
+export const GRPC = "grpc";
+export const GULP = "gulp";
+export const HTML = "html";
+export const JAVASCRIPT = "javascript";
+export const JQUERY = "jquery";
+export const KIBANA = "kibana";
+export const LOGSTASH = "logstash";
 export const MONGODB = "mongodb"
 export const MQTT = "mqtt";
-export const ELASTIC = "elastic";
-export const LOGSTASH = "logstash";
-export const KIBANA = "kibana";
-export const TYPESCRIPT = "typescript";
-export const REACT = "react";
-export const GATSBY = "gatsby";
-export const GRPC = "grpc";
-export const PROTOBUF = "protobuf";
 export const NODE = "node";
-export const JQUERY = "jquery";
-export const CHROMIUM = "chromium";
-export const SENTRY = "sentry";
+export const PROTOBUF = "protobuf";
 export const PUPPETEER = "puppeteer";
+export const REACT = "react";
 export const RXJS = "rxjs";
-export const JAVASCRIPT = "javascript";
-export const CYPRESS = "cypress";
-export const GITHUB_ACTIONS = "github_actions";
-export const GULP = "gulp";
-export const WEBPACK = "webpack";
 export const SASS = "sass";
+export const SENTRY = "sentry";
+export const TYPESCRIPT = "typescript";
 export const W3C = "w3c";
-export const CSS = "css";
-export const HTML = "html";
+export const WEBPACK = "webpack";
 
 export const skillsMap: {
     [skillName: string]: {
@@ -37,8 +37,36 @@ export const skillsMap: {
         imageHeight: number;
     }
 } = {
-    [GOLANG]: {
-        imageUrl: "/images/logo/golang.png",
+    [CHROMIUM]: {
+        imageUrl: "/images/logo/chromium.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [CSS]: {
+        imageUrl: "/images/logo/css.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 20,
+    },
+    [CYPRESS]: {
+        imageUrl: "/images/logo/cypress.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [ECHO]: {
+        imageUrl: "/images/logo/echo.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 22,
+    },
+    [ELASTIC]: {
+        imageUrl: "/images/logo/elastic.png",
         width: -1,
         height: 24,
         imageWidth: -1,
@@ -51,6 +79,27 @@ export const skillsMap: {
         imageWidth: -1,
         imageHeight: 22,
     },
+    [GATSBY]: {
+        imageUrl: "/images/logo/gatsby.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [GITHUB_ACTIONS]: {
+        imageUrl: "/images/logo/github_actions.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [GOLANG]: {
+        imageUrl: "/images/logo/golang.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
     [GOOGLE_CLOUD]: {
         imageUrl: "/images/logo/google_cloud.png",
         width: -1,
@@ -58,12 +107,54 @@ export const skillsMap: {
         imageWidth: -1,
         imageHeight: 20,
     },
-    [ECHO]: {
-        imageUrl: "/images/logo/echo.png",
+    [GRPC]: {
+        imageUrl: "/images/logo/grpc.png",
         width: -1,
         height: 24,
         imageWidth: -1,
-        imageHeight: 22,
+        imageHeight: 20,
+    },
+    [GULP]: {
+        imageUrl: "/images/logo/gulp.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [HTML]: {
+        imageUrl: "/images/logo/html.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 20,
+    },
+    [JAVASCRIPT]: {
+        imageUrl: "/images/logo/javascript.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [JQUERY]: {
+        imageUrl: "/images/logo/jquery.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 18,
+    },
+    [KIBANA]: {
+        imageUrl: "/images/logo/kibana.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
+    [LOGSTASH]: {
+        imageUrl: "/images/logo/logstash.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
     },
     [MONGODB]: {
         imageUrl: "/images/logo/mongodb.png",
@@ -79,29 +170,22 @@ export const skillsMap: {
         imageWidth: -1,
         imageHeight: 17,
     },
-    [ELASTIC]: {
-        imageUrl: "/images/logo/elastic.png",
+    [NODE]: {
+        imageUrl: "/images/logo/node.png",
         width: -1,
         height: 24,
         imageWidth: -1,
-        imageHeight: 24,
+        imageHeight: 22,
     },
-    [LOGSTASH]: {
-        imageUrl: "/images/logo/logstash.png",
+    [PROTOBUF]: {
+        imageUrl: "/images/logo/protobuf.png",
         width: -1,
         height: 24,
         imageWidth: -1,
-        imageHeight: 24,
+        imageHeight: 22,
     },
-    [KIBANA]: {
-        imageUrl: "/images/logo/kibana.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [TYPESCRIPT]: {
-        imageUrl: "/images/logo/typescript.png",
+    [PUPPETEER]: {
+        imageUrl: "/images/logo/puppeteer.png",
         width: -1,
         height: 24,
         imageWidth: -1,
@@ -114,99 +198,8 @@ export const skillsMap: {
         imageWidth: -1,
         imageHeight: 24,
     },
-    [GATSBY]: {
-        imageUrl: "/images/logo/gatsby.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [GRPC]: {
-        imageUrl: "/images/logo/grpc.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 20,
-    },
-    [PROTOBUF]: {
-        imageUrl: "/images/logo/protobuf.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 22,
-    },
-    [NODE]: {
-        imageUrl: "/images/logo/node.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 22,
-    },
-    [JQUERY]: {
-        imageUrl: "/images/logo/jquery.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 18,
-    },
-    [CHROMIUM]: {
-        imageUrl: "/images/logo/chromium.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [SENTRY]: {
-        imageUrl: "/images/logo/sentry.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 20,
-    },
-    [PUPPETEER]: {
-        imageUrl: "/images/logo/puppeteer.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
     [RXJS]: {
         imageUrl: "/images/logo/rxjs.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [JAVASCRIPT]: {
-        imageUrl: "/images/logo/javascript.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [CYPRESS]: {
-        imageUrl: "/images/logo/cypress.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [GITHUB_ACTIONS]: {
-        imageUrl: "/images/logo/github_actions.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [GULP]: {
-        imageUrl: "/images/logo/gulp.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 24,
-    },
-    [WEBPACK]: {
-        imageUrl: "/images/logo/webpack.png",
         width: -1,
         height: 24,
         imageWidth: -1,
@@ -219,6 +212,20 @@ export const skillsMap: {
         imageWidth: -1,
         imageHeight: 20,
     },
+    [SENTRY]: {
+        imageUrl: "/images/logo/sentry.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 20,
+    },
+    [TYPESCRIPT]: {
+        imageUrl: "/images/logo/typescript.png",
+        width: -1,
+        height: 24,
+        imageWidth: -1,
+        imageHeight: 24,
+    },
     [W3C]: {
         imageUrl: "/images/logo/w3c.png",
         width: -1,
@@ -226,18 +233,11 @@ export const skillsMap: {
         imageWidth: -1,
         imageHeight: 18,
     },
-    [CSS]: {
-        imageUrl: "/images/logo/css.png",
+    [WEBPACK]: {
+        imageUrl: "/images/logo/webpack.png",
         width: -1,
         height: 24,
         imageWidth: -1,
-        imageHeight: 20,
+        imageHeight: 24,
     },
-    [HTML]: {
-        imageUrl: "/images/logo/html.png",
-        width: -1,
-        height: 24,
-        imageWidth: -1,
-        imageHeight: 20,
-    }
 }
