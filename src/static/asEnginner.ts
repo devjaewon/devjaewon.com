d35dc2583f665e86d4b5a80b1fd2282431852fe7
devjoeween <jjazan201@gmail.com>
2023-02-09 19:39:18 +0900

    feat(home): update as engineer

--- src/static/asEnginner.ts
+++ src/static/asEnginner.ts
@@ -1,7 +1,7 @@
 import type { AsEngineer } from "@/types/struct";
 
 export const asEngineer: Array<AsEngineer> = [
-    { text: "기술 자체보다 실효성과 고객을 바라봅니다" },
-    { text: "지표 기반의 지속적인 개선을 추구합니다" },
+    { text: "기술의 원리와 탐구도 중요하지만, 실효성과 고객을 바라보려 합니다" },
+    { text: "지표 기반의 지속적인 개선을 고민합니다" },
     { text: "제품의 성장과 함께 팀의 유대와 성장 또한 동반되어야 한다고 생각합니다" },
 ];
