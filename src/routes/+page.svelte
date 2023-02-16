a79f293a0320ad63278d40e9d8e3dba87a3bce1a
devjaewon <jjazan201@gmail.com>
2023-02-16 17:55:08 +0900

    feat(home): add meta tags for SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
+<svelte:head>
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://devjaewon.com">
+    <meta property="og:title" content="DEVJAEWON">
+    <meta property="og:image" content="https://devjaewon.com/images/og_image.jpg">
+    <meta property="og:description" content="소프트웨어 엔지니어의 일상을 기록합니다">
+    <meta property="og:site_name" content="DEVJAEWON">
+    <meta property="og:locale" content="ko_KR">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <title>DEVJAEWON</title>
+</svelte:head>
 <App />
 
 <script>
